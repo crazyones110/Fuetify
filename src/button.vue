d2060304d324207c00392271603bf31cbfcceb8e
crazyones110 <crazyones110@gmail.com>
2020-01-04 23:02:48 +0800

    加入parcel，将button组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class='f-button'>你好</button>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='scss'>
+.f-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em; /*左边一个字，右边一个字*/
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+
+  &:hover{
+    border-color: var(--border-color-hover);
+  }
+  &:active{
+    border-color: var(--border-active-bg);
+  }
+  &:focus{
+    outline: none;
+  }
+}
+</style>
