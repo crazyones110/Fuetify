03d2ae0a0a9d052ed3ec01a4cb3aef76b6a3a3ff
crazyones110 <crazyones110@gmail.com>
2020-01-29 22:57:43 +0800

    基本实现 row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div
+    class="col"
+    :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+    :style="{paddingLeft: gutter/2+ 'px', paddingRight: gutter/2+'px'}"
+  >
+    <div style="border: 1px solid green;height: 100px">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,19 +16,36 @@ export default {
   props: {
     span: {
       type: [Number, String]
+    },
+    offset: [Number, String]
+  },
+  data() {
+    return {
+      gutter: 0
     }
+  },
+  created() { // 这个时候没有儿子
+    console.log('col created')
+  },
+  mounted() { // 这个时候有儿子
+    console.log('col mounted')
   }
 }
 </script>
 
 <style lang="sass" scoped>
 .col
-  height: 100px
-  background-color: grey
+  // height: 100px
+  // background-color: grey
   width: 50%
-  border: 1px solid red
+  // border: 1px solid red
+  // padding: 0 10px
   $class-prefix: col-
   @for $n from 1 through 24
     &.#{$class-prefix}#{$n}
       width: ($n / 24) * 100%
+  $class-prefix: offset-
+  @for $n from 1 through 24
+    &.#{$class-prefix}#{$n}
+      margin-left: ($n / 24) * 100%
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,16 +1,44 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2+ 'px', marginRight: -gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'FRow',
+  props: {
+    gutter: {
+      type: [Number, String],
+      default: 0
+    }
+  },
+  created() { // 这个时候没有儿子
+    console.log('row created')
+  },
+  beforeMount() {
+    console.log('row beforeMount')
+    console.log(this.$children)
+  },
+  mounted() { // 这个时候有儿子
+    console.log('row mounted')
+    this.$children.forEach(vm => {
+      // console.log(vm)
+      vm.gutter = this.gutter
+    })
+  }
 }
+
+const div = document.createElement('div') // created 在内存中生成这个对象
+const childDiv = document.createElement('div') // child created
+// 但其实儿子和父亲谁先 created 无所谓的，Vue 选择了哪一种呢
+div.appendChild(childDiv) // child mounted
+document.body.appendChild(div) // mounted 把这个 div 对象挂到页面里面去
+
 </script>
 
 <style lang="sass" scoped>
   .row
     display: flex
+    // margin: 0 -10px
 </style>
