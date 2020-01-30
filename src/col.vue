16037cff79485f5ab18a939dfc223cad7a462bee
crazyones110 <crazyones110@gmail.com>
2020-01-30 13:58:23 +0800

    响应式基本完成

--- src/col.vue
+++ src/col.vue
@@ -71,7 +71,6 @@ export default {
 
 <style lang="sass" scoped>
 .col
-  // width: 50%
   $class-prefix: col-
   @for $n from 1 through 24
     &.#{$class-prefix}#{$n}
@@ -81,7 +80,7 @@ export default {
     &.#{$class-prefix}#{$n}
       margin-left: ($n / 24) * 100%
 
-  @media (min-width: 577px) and (max-width: 768px)
+  @media (min-width: 577px)
     $class-prefix: col-ipad-
     @for $n from 1 through 24
       &.#{$class-prefix}#{$n}
@@ -91,7 +90,7 @@ export default {
       &.#{$class-prefix}#{$n}
         margin-left: ($n / 24) * 100%
 
-  @media (min-width: 769px) and (max-width: 992px)
+  @media (min-width: 769px)
     $class-prefix: col-narrow-pc-
     @for $n from 1 through 24
       &.#{$class-prefix}#{$n}
@@ -101,7 +100,7 @@ export default {
       &.#{$class-prefix}#{$n}
         margin-left: ($n / 24) * 100%
 
-  @media (min-width: 993px) and (max-width: 1200px)
+  @media (min-width: 993px)
     $class-prefix: col-pc-
     @for $n from 1 through 24
       &.#{$class-prefix}#{$n}
