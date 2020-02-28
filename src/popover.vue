2cc7b5f85503107be85bd512aae48f3abfba98e0
crazyones110 <crazyones110@gmail.com>
2020-02-28 23:18:05 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display: inline-block">
       <slot></slot>
     </span>
   </div>
@@ -29,13 +29,13 @@ export default {
       this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
       this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
     },
-    eventHandler(e) {
+    onClickDocument(e) {
       if (this.$refs.popover.contains(e.target)) { return }
       if (this.$refs.contentWrapper.contains(e.target)) { return }
       this.close()
     },
     listenToDocument() {
-      document.addEventListener("click", this.eventHandler)
+      document.addEventListener("click", this.onClickDocument)
     },
     open() {
       this.visible = true
@@ -47,7 +47,7 @@ export default {
     close() {
       this.visible = false
       console.log('关闭')
-      document.removeEventListener("click", this.eventHandler)
+      document.removeEventListener("click", this.onClickDocument)
     },
     onClick(e) {
       if (this.$refs.triggerWrapper.contains(e.target)) {
@@ -63,6 +63,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   vertical-align: top;
@@ -70,8 +72,33 @@ export default {
 }
 .content-wrapper {
   position: absolute;
-  border: 1px solid red;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+  // box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+  background-color: white;
   transform: translateY(-100%);
+  margin-top: -10px;
+  padding: .5em 1em;
+  max-width: 20em;
+  min-width: 3em;
+  word-break: break-all;
+  &::before,&::after {
+    content: '';
+    display: block;
+    width: 0px;
+    height: 0px;
+    position: absolute;
+    left: 10px;
+    border: 10px solid transparent;
+  }
+  &::before {
+    border-top-color: black;
+    top: 100%;
+  }
+  &::after {
+    border-top-color: white;
+    top: calc(100% - 1px);
+  }
 }
 </style>
